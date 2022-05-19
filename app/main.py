01629e549b08424149614f482d8babcd3ef04ed1
Dheeraj Lakhani <dheeraj>
2022-05-19 15:44:12 +0530

    changing the documentation

--- app/main.py
+++ app/main.py
@@ -18,9 +18,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(post.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(post.router)
 app.include_router(vote.router)
 
 @app.get("/")
